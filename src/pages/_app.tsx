544084fec1155b8da426042afa90a5cd867e142c
c1ph3r <gabrielcristianoluttjohann@gmail.com>
2024-10-18 18:11:55 -0300

    new color

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,15 @@
 import React from "react";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-
 import Cursor from "@/components/sections/Cursor/Cursor";
 
 import "bootstrap/dist/css/bootstrap.min.css";
-import "@/assets/css/bootstrap-modifications.css";
-import "@/assets/css/fontawesome-all.min.css";
 import "@/assets/css/style.css";
 import "@/assets/css/spacing.css";
-import "@/assets/css/mouse-effect.css";
+import "@/assets/css/bootstrap-modifications.css";
+import "@/assets/css/fontawesome-all.min.css";
 import "@/assets/css/animate.min.css";
+import "@/assets/css/mouse-effect.css";
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
