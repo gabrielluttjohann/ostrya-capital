860d95714cee7380a5d47d2228cd6817954ed1be
c1ph3r <gabrielcristianoluttjohann@gmail.com>
2024-10-20 18:38:38 -0300

    .

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,12 +4,16 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>{/* Add custom scripts or styles here */}</Head>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Brygada+1918:wght@400;500;600;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
         </body>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/rellax/1.12.1/rellax.min.js"></script>
       </Html>
     );
   }
